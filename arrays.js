8a8f1a5579aa921b7b83ed458cd85dc1c3798c01
Jordan Andrew Thomas <thomas.jordan.andrew@gmail.com>
2018-07-03 08:03:46 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -8,6 +8,5 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array];
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array = array.unshift(element);
-  return array;
+  return array.unshift(element);
 }
