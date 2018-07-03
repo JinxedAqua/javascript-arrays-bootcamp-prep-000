46481e233087f65360a6f2631e8b507c9d1fde24
Jordan Andrew Thomas <thomas.jordan.andrew@gmail.com>
2018-07-03 08:10:27 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -12,8 +12,9 @@ function destructivelyAddElementToBeginningOfArray(array, element){
   return array;
 }
 function addElementToEndOfArray(array, element){
-  
+  return [...array, element];
 }
-function addElementToEndOfArray(array, element){
-  
+function destructivelyAddElementToEndOfArray(array, element){
+  array.push(element);
+  return array;
 }
