49b00d036973e50e3f84020fd8ffb250f596e397
Jordan Andrew Thomas <thomas.jordan.andrew@gmail.com>
2018-07-03 07:55:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -5,7 +5,7 @@ var chocolateBars = [
   'skittles'
   ];
 function addElementToBeginningOfArray(array, element){
-  array.unshift(element);
+  array = array.unshift(element);
   return array;
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
